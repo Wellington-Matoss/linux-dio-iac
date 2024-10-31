9e78216f6384330d7ae657691aa5b87c4a980c5b
Wellington Matos <wellington.matos.neves@gmail.com>
2024-10-31 10:36:50 -0300

    Atualiza script de criação de diretórios, grupos e usuários com novas permissões e usuários

--- iac1.sh
+++ iac1.sh
@@ -2,49 +2,49 @@
 
 echo "Criando diretórios..."
 
-mkdir /teyvat
-mkdir /arconte
-mkdir /gaso
-mkdir /cute
+mkdir /publico
+mkdir /adm
+mkdir /ven
+mkdir /sec
 
 echo "Criando grupos de usuários..."
 
-groupadd GRP_ARC
-groupadd GRP_GAS
-groupadd GRP_CUT
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
 
 echo "Criando usuários..."
 
-useradd furina -m -s /bin/bash -G GRP_ARC
-echo "furina:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd raiden -m -s /bin/bash -G GRP_ARC
-echo "raiden:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd murata -m -s /bin/bash -G GRP_ARC
-echo "murata:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd carlos -m -s /bin/bash -G GRP_ARC
+echo "carlos:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd maria -m -s /bin/bash -G GRP_ARC
+echo "maria:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd joao -m -s /bin/bash -G GRP_ARC
+echo "joao:$(openssl passwd -6 20152016)" | sudo chpasswd
 
-useradd clorinde -m -s /bin/bash -G GRP_GAS
-echo "clorinde:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd sara -m -s /bin/bash -G GRP_GAS
-echo "sara:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd xilonen -m -s /bin/bash -G GRP_GAS
-echo "xilonen:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd debora -m -s /bin/bash -G GRP_GAS
+echo "debora:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd sebastiana -m -s /bin/bash -G GRP_GAS
+echo "sebastiana:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd roberto -m -s /bin/bash -G GRP_GAS
+echo "roberto:$(openssl passwd -6 20152016)" | sudo chpasswd
 
-useradd chiori -m -s /bin/bash -G GRP_CUT
-echo "chiori:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd sayu -m -s /bin/bash -G GRP_CUT
-echo "sayu:$(openssl passwd -6 20152016)" | sudo chpasswd
-useradd kachina -m -s /bin/bash -G GRP_CUT
-echo "kachina:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd josefina -m -s /bin/bash -G GRP_CUT
+echo "josefina:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd amanda -m -s /bin/bash -G GRP_CUT
+echo "amanda:$(openssl passwd -6 20152016)" | sudo chpasswd
+useradd rogerio -m -s /bin/bash -G GRP_CUT
+echo "rogerio:$(openssl passwd -6 20152016)" | sudo chpasswd
 
 echo "Especificando permissões dos diretórios..."
 
-chown root:GRP_ARC /arconte
-chown root:GRP_GAS /gaso
-chown root:GRP_CUT /cute
+chown root:GRP_ADM /adm
+chown root:GRP_VEN /ven
+chown root:GRP_SEC /sec
 
-chmod 770 /arconte
-chmod 770 /gaso
-chmod 770 /cute
-chmod 777 /teyvat
+chmod 770 /adm
+chmod 770 /ven
+chmod 770 /sec
+chmod 777 /publico
 
-echo "Finalizdo!"
+echo "IaC Finalizdo!"
